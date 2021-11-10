40108c16bb0a2ac0fcaf5726c7865bc89878e45a
Boris Besemer <borisbesemer@gmail.com>
2021-11-10 15:56:18 +0100

    feat(terraform): add support for schemaDirectory and hostname

--- packages/amplience-terraform/examples/output/bla.tf
+++ packages/amplience-terraform/examples/output/bla.tf
@@ -1,8 +1,12 @@
-terraform {
-  required_version = ">= 0.12"
+resource "amplience_content_type_schema" "test" {
+  body             = file("${path.module}/content-type-schemas/schemas/test.json")
+  schema_id        = "https://schema-examples.com/test"
+  validation_level = "CONTENT_TYPE"
 }
 
-resource "amplience_content_type_schema" "test" {
-  validation_level = "CONTENT_TYPE"
+resource "amplience_content_type" "test" {
+  content_type_uri = amplience_content_type_schema.test.schema_id
+  label            = "Test"
+  status           = "ACTIVE"
 }
 
--- packages/amplience-terraform/src/index.ts
+++ packages/amplience-terraform/src/index.ts
@@ -5,52 +5,68 @@ import {
   PluginValidateFn,
   Types,
 } from '@graphql-codegen/plugin-helpers';
-import { paramCase, snakeCase } from 'change-case';
-import {
-  FieldDefinitionNode,
-  GraphQLSchema,
-  ObjectTypeDefinitionNode,
-  visit,
-} from 'graphql';
+import { snakeCase, paramCase, capitalCase } from 'change-case';
+import { GraphQLSchema, ObjectTypeDefinitionNode, visit } from 'graphql';
 import { extname } from 'path';
-import { TerraformGenerator } from 'terraform-generator';
+import { TerraformGenerator, fn } from 'terraform-generator';
 
 type Config = {
   hostname: string;
+  schemaDirectory: string;
 };
 
 export const plugin: PluginFunction<any> = (
   schema: GraphQLSchema,
   _documents: Types.DocumentFile[],
-  _config: Config
+  config: Config
 ) => {
   const astNode = getCachedDocumentNodeFromSchema(schema);
-  const tfg = new TerraformGenerator({
-    required_version: '>= 0.12',
-  });
+  const tfg = new TerraformGenerator();
 
+  /**
+   * Visitor is only used to add resources to the TerraformGenerator,
+   * it doesn't return anything itself
+   */
   const visitor = {
-    FieldDefinition: (node: FieldDefinitionNode) => {
-      return {
-        [node.name.value]: {
-          title: paramCase(node.name.value),
-        },
-      };
-    },
     ObjectTypeDefinition: (node: ObjectTypeDefinitionNode) => {
-      tfg.resource(
-        'amplience_content_type_schema',
-        snakeCase(node.name.value),
-        { validation_level: 'CONTENT_TYPE' }
-      );
-      return tfg.generate().tf;
+      const snakeName = snakeCase(node.name.value);
+      const paramName = paramCase(node.name.value);
+      if (
+        node.directives?.find(
+          directive => directive.name.value === 'amplienceContent'
+        )
+      ) {
+        const schema = tfg.resource(
+          'amplience_content_type_schema',
+          snakeName,
+          {
+            body: fn(
+              'file',
+              `${config.schemaDirectory ?? '.'}/${paramName}.json`
+            ),
+            schema_id: `${config.hostname}/${paramName}`,
+            validation_level: 'CONTENT_TYPE',
+          }
+        );
+
+        tfg.resource('amplience_content_type', snakeName, {
+          content_type_uri: schema.attr('schema_id'),
+          label: capitalCase(node.name.value),
+          status: 'ACTIVE',
+        });
+      }
+      return null;
     },
   };
 
-  const result = visit(astNode, { leave: visitor });
+  visit(astNode, { leave: visitor });
 
-  return result.definitions[0];
+  return tfg.generate().tf;
 };
+
+export const addToSchema = `
+directive @amplienceContent on OBJECT
+`;
 
 export const validate: PluginValidateFn<any> = async (
   _schema: GraphQLSchema,
@@ -58,6 +74,7 @@ export const validate: PluginValidateFn<any> = async (
   config: Config,
   outputFile: string
 ) => {
+  // Hostname is used for setting up
   if (config.hostname === undefined) {
     throw new Error(
       `Plugin "amplience" requires a hostname to be set in the config!`
