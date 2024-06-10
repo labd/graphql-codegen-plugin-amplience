310f2e4e0c515378a3fe5a3dc52f747300e2887b
jonnoallcock <jonathan.allcock@frasers.group>
2024-06-10 17:13:57 +0100

    fix(json): lint + prettier + typecheck

--- packages/plugin-json/src/lib/types.ts
+++ packages/plugin-json/src/lib/types.ts
@@ -1,12 +1,67 @@
+export type RefType = { allOf: Array<{ $ref: string } | object> };
+
+export type LocalizedType = RefType & {
+  properties: {
+    values: {
+      items: {
+        properties: {
+          value: AmpliencePropertyType;
+        };
+      };
+    };
+  };
+};
+
+export type SortableTrait = {
+  sortBy: {
+    key: string;
+    paths: string[];
+  }[];
+};
+
+export type HierarchyTrait = {
+  childContentTypes: string[] | undefined;
+};
+
+export type FilterableTrait = {
+  filterBy: {
+    paths: string[];
+  }[];
+};
+
+export type EnumProperties = {
+  properties: {
+    contentType: {
+      enum: string[];
+    };
+  };
+};
+
+export type InlineObject = {
+  type: string;
+  properties: Properties;
+  propertyOrder: string[] | undefined;
+  required: string[] | undefined;
+};
+
+export type InlineContentReference = {
+  type: "object";
+  allOf: RefType["allOf"];
+};
+
+export type Properties = {
+  [name: string]: AmpliencePropertyType;
+};
+
 export interface AmpliencePropertyType {
   title?: string;
   description?: string;
   type?: string;
-  const?: any;
+  const?: string | string[];
   enum?: string[];
-  allOf?: { [key: string]: any }[];
+  allOf?: RefType["allOf"];
   items?: AmpliencePropertyType;
-  properties?: any;
+  properties?: Properties;
   propertyOrder?: string[];
   required?: string[];
   format?: string;
@@ -20,29 +75,34 @@ export interface AmpliencePropertyType {
 }
 
 export type AmplienceDeliveryKeyType = {
-  type: 'object',
-  title: 'Delivery Key'
+  type: "object";
+  title: "Delivery Key";
   properties: {
     deliveryKey: {
-      type: 'string'
-      title: string
-      description: string
-      pattern?: string
-    }
-  }
-}
+      type: "string";
+      title: string;
+      description: string;
+      pattern?: string;
+    };
+  };
+};
 
 export interface AmplienceContentTypeSchemaBody {
   $id: string;
   $schema: string;
-  allOf: any[];
+  allOf?: RefType["allOf"][number];
   title: string;
   description: string;
   "trait:filterable"?: {};
   "trait:hierarchy"?: {};
   "trait:sortable"?: {};
   type: "object";
-  properties?: {[name: string | '_meta']: AmpliencePropertyType | AmplienceDeliveryKeyType | undefined};
+  properties?: {
+    [name: string | "_meta"]:
+      | AmpliencePropertyType
+      | AmplienceDeliveryKeyType
+      | undefined;
+  };
   definitions?: { [name: string]: AmpliencePropertyType };
   propertyOrder?: string[];
   required?: string[];
--- packages/plugin-json/test/validate.test.ts
+++ packages/plugin-json/test/validate.test.ts
@@ -1,7 +1,7 @@
-import fs from "fs";
-import { gql } from "graphql-tag";
-import { buildASTSchema, buildSchema, print } from "graphql";
 import { schemaPrepend } from "amplience-graphql-codegen-common";
+import fs from "fs";
+import { buildASTSchema, buildSchema, print } from "graphql";
+import { gql } from "graphql-tag";
 import { addToSchema, validate } from "../src";
 
 it.each([{ graphqlFile: "base" }, { graphqlFile: "hierarchy" }])(
@@ -9,10 +9,10 @@ it.each([{ graphqlFile: "base" }, { graphqlFile: "hierarchy" }])(
   ({ graphqlFile }) => {
     const schema = buildSchema(
       addToSchema +
-        fs.readFileSync(`./test/testdata/${graphqlFile}.graphql`, "utf8")
+        fs.readFileSync(`./test/testdata/${graphqlFile}.graphql`, "utf8"),
     );
     expect(validate(schema, [], {}, "", [])).toBe(undefined);
-  }
+  },
 );
 
 it("Throws Error: Fields with '@amplienceLocalized' must be Nullable", () => {
@@ -30,7 +30,7 @@ it("Throws Error: Fields with '@amplienceLocalized' must be Nullable", () => {
     }
   `);
   expect(() => validate(schema, [], {}, "", [])).toThrow(
-    "Fields with '@amplienceLocalized' must be Nullable.\n\ntype Test\n\tinvalidLocalizedProp\n\tinvalidLocalizedListProp2\n\tinvalidLocalizedListProp"
+    "Fields with '@amplienceLocalized' must be Nullable.\n\ntype Test\n\tinvalidLocalizedProp\n\tinvalidLocalizedListProp2\n\tinvalidLocalizedListProp",
   );
 });
 
@@ -48,7 +48,7 @@ it("Throws Error: Types can have no more than 5 fields with '@amplienceFiltered'
     }
   `);
   expect(() => validate(schema, [], {}, "", [])).toThrow(
-    "Types can have no more than 5 fields with '@amplienceFiltered'.\n\ntype Test\n\ta\n\tb\n\tc\n\td\n\te\n\tf"
+    "Types can have no more than 5 fields with '@amplienceFiltered'.\n\ntype Test\n\ta\n\tb\n\tc\n\td\n\te\n\tf",
   );
 });
 
@@ -61,60 +61,63 @@ it("Throws error: Types can only have 1 field with '@amplienceDeliveryKey'", () 
     }
   `);
   expect(() => validate(schema, [], {}, "", [])).toThrow(
-    "Types can only have 1 field with '@amplienceDeliveryKey'.\n\ntype Test\n\ta\n\tb"
+    "Types can only have 1 field with '@amplienceDeliveryKey'.\n\ntype Test\n\ta\n\tb",
   );
 });
 
 it.each([
   gql`
-  type Test {
-    a: String! @amplienceDeliveryKey
-  }
+    type Test {
+      a: String! @amplienceDeliveryKey
+    }
   `,
   gql`
-  type Test {
-    a: Int! @amplienceDeliveryKey
-  }
+    type Test {
+      a: Int! @amplienceDeliveryKey
+    }
   `,
   gql`
-  type Test {
-    a: Float! @amplienceDeliveryKey
-  }
+    type Test {
+      a: Float! @amplienceDeliveryKey
+    }
   `,
   gql`
-  type Test {
-    a: Boolean! @amplienceDeliveryKey
-  }
+    type Test {
+      a: Boolean! @amplienceDeliveryKey
+    }
   `,
   gql`
-  type Test {
-    a: Int @amplienceDeliveryKey
-  }
+    type Test {
+      a: Int @amplienceDeliveryKey
+    }
   `,
   gql`
-  type Test {
-    a: Float @amplienceDeliveryKey
-  }
+    type Test {
+      a: Float @amplienceDeliveryKey
+    }
   `,
   gql`
-  type Test {
-    a: Boolean @amplienceDeliveryKey
-  }
+    type Test {
+      a: Boolean @amplienceDeliveryKey
+    }
   `,
   gql`
-  type ObjectType {
-    a: String!
-  }
-  type Test {
-    a: ObjectType @amplienceDeliveryKey
-  }
-  `
-])("Throws error: Fields with '@amplienceDeliveryKey' must be of Nullable type String", (testSchema) => {
-  const schema = buildASTSchema(gql`
-    ${print(schemaPrepend)}
-    ${print(testSchema)}
-  `);
-  expect(() => validate(schema, [], {}, "", [])).toThrow(
-    "Fields with '@amplienceDeliveryKey' must be of Nullable type String.\n\ntype Test\n\ta"
-  );
-});
+    type ObjectType {
+      a: String!
+    }
+    type Test {
+      a: ObjectType @amplienceDeliveryKey
+    }
+  `,
+])(
+  "Throws error: Fields with '@amplienceDeliveryKey' must be of Nullable type String",
+  (testSchema) => {
+    const schema = buildASTSchema(gql`
+      ${print(schemaPrepend)}
+      ${print(testSchema)}
+    `);
+    expect(() => validate(schema, [], {}, "", [])).toThrow(
+      "Fields with '@amplienceDeliveryKey' must be of Nullable type String.\n\ntype Test\n\ta",
+    );
+  },
+);
