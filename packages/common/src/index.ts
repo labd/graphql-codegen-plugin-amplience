68fbbf1a4f811be7fabe54102cb7730421b175e7
jonnoallcock <jonathan.allcock@frasers.group>
2024-06-10 17:04:41 +0100

    fix(common): lint + prettier + typecheck

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,9 +1,9 @@
 import { paramCase } from "change-case";
-import { TypeDefinitionNode } from "graphql";
+import type { TypeDefinitionNode } from "graphql";
 
 export * from "./graphql";
 export * from "./util";
 export * from "./directives";
 
-export const typeUri = (type: TypeDefinitionNode, schemaHost: string) =>
+export const typeUri = (type: TypeDefinitionNode, schemaHost: string): string =>
   `${schemaHost}/${paramCase(type.name.value)}`;
